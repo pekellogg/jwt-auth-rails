4ba11250e8e249f399d32d6b91d4416d39ffc635
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-17 20:21:15 -0800

    done.

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,46 @@
 class Api::V1::UsersController < ApplicationController
+  # skip the before_action :authorized coming from ApplicationController
+  # don't ask users to login before they create an account
   skip_before_action :authorized, only: [:create]
 
   def profile
-    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    render json: { 
+              user: UserSerializer.new(current_user)
+            }, 
+            status: :accepted
   end
+
+  # corresponding client-side fetch for successful create
+  # fetch("http://localhost:3000/api/v1/users", {
+  #   method: "POST",
+  #   headers: {
+  #     "Content-Type": "application/json",
+  #     Accept: "application/json",
+  #   },
+  #   body: JSON.stringify(newUserData),
+  # })
+  # .then((r) => r.json())
+  # .then((data) => {
+  #   # save the token to localStorage for future access
+  #   localStorage.setItem("jwt", data.jwt);
+  #   # save the user somewhere (in state!) to log the user in
+  #   setUser(data.user);
+  # });
 
   def create
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token({ user_id: @user.id })
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      render json: { 
+              user: UserSerializer.new(@user),
+              jwt: @token
+            }, 
+            status: :created
     else
-      render json: { error: 'failed to create user' }, status: :unprocessable_entity
+      render json: { 
+              error: 'failed to create user'
+            }, 
+            status: :unprocessable_entity
     end
   end
 
--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,19 +1,36 @@
 class ApplicationController < ActionController::API
+  # call the authorized method before anything else happens in our app. 
   before_action :authorized
 
   def encode_token(payload)
     # don't forget to hide your secret in an environment variable
+    # payload => { beef: 'steak' }
     JWT.encode(payload, 'my_s3cr3t')
+    # jwt string: "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI"
   end
 
   def auth_header
+    # { 'Authorization': 'Bearer <token>' }
     request.headers['Authorization']
   end
 
+  # sample corresponding fetch request
+  # fetch("http://localhost:3000/api/v1/profile", {
+  #   method: "GET",
+  #   headers: {
+  #     Authorization: `Bearer <token>`,
+  #   },
+  # });
+
   def decoded_token
+    # token => "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI"
     if auth_header
       token = auth_header.split(' ')[1]
+      # header: { 'Authorization': 'Bearer <token>' }
       begin
+        # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
+        # JWT.decode(token, 'my_s3cr3t')[0]
+        # [0] gives us the payload { "beef"=>"steak" }
         JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
       rescue JWT::DecodeError
         nil
